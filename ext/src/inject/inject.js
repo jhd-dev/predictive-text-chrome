d32a1e6f3ecdefb09c48a6d87116242422430f42
Jonathan Doliver <porygonj@gmail.com>
2017-09-11 19:04:59 -0400

    add element to page

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
+$(document).ready(() => {
 
-	//$('input[type=text], textarea')
-	alert("hi");
+	let $predictionList = $('<span id="prediction-list">hello</span>');
+
+	$('body').append($predictionList);
+
+	$(':input').on({
+		focus(){
+			$predictionList.addClass('visible');
+		},
+		blur(){
+			$predictionList.removeClass('visible');
+		},
+		input(){
+			const oldValue = $(this).val();
+			$(this).val(oldValue);
+		}
+	})
 
 });
